993c9d538b105ecb2ffc722558d4041e18e20f95
Wiktor <wiktorkar25@gmail.com>
2024-10-11 11:31:29 +0200

    Add callback setup in Window.hpp through events

--- App/inc/Window.hpp
+++ App/inc/Window.hpp
@@ -1,26 +1,30 @@
 #ifndef _WINDOW_HPP_
 #define _WINDOW_HPP_
 
-#include "GLFW/glfw3.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
 #include <functional>
 #include <string_view>
 #include "fmt/color.h"
+#include "Events.hpp"
 
 struct Properties
 {
     std::string title;
     int width;
     int height;
+    std::function<void(Event&)> callback;
 };
 
-class Event;
 
 static bool is_GLFW_initialized = false;
+static bool is_GLEW_initialized = false;
 
 class Window
 {
 public:
-    Window(const Properties& properties = {"3D Editor", 960, 540})
+    Window(const Properties& properties = {"3D Editor", 960, 540, nullptr})
     : m_properties {properties}
     {
 
@@ -42,6 +46,8 @@ public:
             glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
             glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
             glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+            // below?
         }
         
         m_window = glfwCreateWindow(
@@ -54,33 +60,49 @@ public:
     
         if (!m_window) {
             fmt::print(fg(fmt::color::red), "Failed to initialize Window!\n");
+            return;
         }
 
         glfwMakeContextCurrent(m_window);
         glfwSetWindowUserPointer(m_window, &m_properties);
 
         fmt::print("Window created!\n");
+
+        // GLEW setup
+        if (!is_GLEW_initialized)
+        {
+            if (glewInit() != GLEW_OK) 
+            {
+                fmt::print(fg(fmt::color::red), "Failed to initialize GLEW!");
+                return;
+            }
+        }
+       
+
+       setWindowCallbacks();
+
     }
 
     auto setEventCallback(std::function<void(Event&)> callback) -> void
     {
-        // callback setup
+        m_properties.callback = callback;
     }
 
-    auto onUpdate()
+    auto onUpdate() -> void
     {
         glfwPollEvents();
         glfwSwapBuffers(m_window);
     }
 
+
     auto getWidth() const noexcept -> int
     {
-        m_properties.width;
+        return m_properties.width;
     }
 
     auto getHeight() const noexcept -> int
     {
-        m_properties.height;
+        return m_properties.height;
     }
 
     auto getWindow() -> GLFWwindow*
@@ -92,6 +114,104 @@ public:
     {
         glfwDestroyWindow(m_window);
     }
+
+
+private:
+    auto setWindowCallbacks() -> void
+    {
+        glfwSetWindowSizeCallback(m_window, [](GLFWwindow* window, int width, int height)
+        {
+            // getting data back from glfw window
+            auto window_data { static_cast<Properties*>(glfwGetWindowUserPointer(window)) };
+            window_data->width  = width;
+            window_data->height = height;
+
+            WindowResizeEvent event {width, height};
+
+            window_data->callback(event);
+        });
+
+
+        glfwSetWindowCloseCallback(m_window, [](GLFWwindow* window){
+            auto window_data { static_cast<Properties*>(glfwGetWindowUserPointer(window)) };
+            WindowCloseEvent event{};
+            window_data->callback(event);
+        });
+
+
+        glfwSetKeyCallback(m_window, [](GLFWwindow *window, int key, int scancode, int action, int mods)
+        {
+            auto window_data { static_cast<Properties*>(glfwGetWindowUserPointer(window)) };
+            
+            switch (action)
+            {
+                case GLFW_PRESS:
+                {
+                    KeyPressedEvent event {key, 0};
+                    window_data->callback(event);
+                }
+                break;
+                case GLFW_RELEASE:
+                {
+                    KeyReleasedEvent event {key};
+                    window_data->callback(event);
+                    break;
+                }
+                case GLFW_REPEAT:
+                {
+                    KeyPressedEvent event {key, 1};
+                    window_data->callback(event);
+                    break;
+                }
+            }
+        });
+
+
+        glfwSetMouseButtonCallback(m_window, [](GLFWwindow *window, int button, int action, int mods)
+        {
+            auto window_data { static_cast<Properties*>(glfwGetWindowUserPointer(window)) };
+            
+            switch (action)
+            {
+                case GLFW_PRESS:
+                {
+
+                    MouseButtonPressedEvent event {button};
+                    window_data->callback(event);                
+                    break;
+                }
+                case GLFW_RELEASE:
+                {
+                    MouseButtonReleasedEvent event {button};
+                    window_data->callback(event);
+                    break;
+                }
+                default:
+                    break;
+            }   
+        });
+
+
+        glfwSetScrollCallback(m_window, [](GLFWwindow *window, double xoffset, double yoffset)
+        {
+            auto window_data { static_cast<Properties*>(glfwGetWindowUserPointer(window)) };
+
+            MouseScrolledEvent event { static_cast<float>(xoffset),static_cast<float>(yoffset) };
+
+            window_data->callback(event);
+        });
+
+
+        glfwSetCursorPosCallback(m_window, [](GLFWwindow *window, double xpos, double ypos)
+        {
+            auto window_data { static_cast<Properties*>(glfwGetWindowUserPointer(window)) };
+
+            MouseMovedEvent event {xpos, ypos};
+
+            window_data->callback(event);
+        });
+    }
+
 private:
     Properties m_properties;
     GLFWwindow* m_window;
