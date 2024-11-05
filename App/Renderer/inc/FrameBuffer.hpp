b39ea8b293d75ddb3c5a443598899d1446e5d889
Wiktor <wiktorkar25@gmail.com>
2024-11-06 00:00:45 +0100

    Remove stencil buffer from framebuffer

--- App/Renderer/inc/FrameBuffer.hpp
+++ App/Renderer/inc/FrameBuffer.hpp
@@ -94,12 +94,12 @@ private:
         glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, m_color_attachment, 0);  
 
 
-        // depth and stencil
+        // depth 
         glGenTextures(1, &m_depth_attachment);
         glBindTexture(GL_TEXTURE_2D, m_depth_attachment);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, m_width, m_height, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, nullptr);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT24, m_width, m_height, 0, GL_DEPTH_COMPONENT, GL_UNSIGNED_BYTE, nullptr);
 
-        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_DEPTH24_STENCIL8, m_depth_attachment, 0);
+        glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, m_depth_attachment, 0);
        
         if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
         {
