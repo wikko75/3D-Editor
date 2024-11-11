211a24453b53b528d56ca169bb3b1e4214d51f29
Wiktor <wiktorkar25@gmail.com>
2024-11-11 21:03:13 +0100

    Add Mesh drawing in Renderer, enable depth test functionality

--- App/Renderer/inc/Renderer.hpp
+++ App/Renderer/inc/Renderer.hpp
@@ -2,8 +2,7 @@
 #define _RENDERER_HPP_
 
 #include "GLFW/glfw3.h"
-#include "VertexArray.hpp"
-#include "Shader.hpp"
+#include "Mesh.hpp"
 #include "fmt/color.h"
 #include "glm/vec4.hpp"
 
@@ -12,12 +11,18 @@ class Renderer
 public:
 
     Renderer(GLFWwindow *window)
-    : m_window {window}
     {
         if (!window) {
             fmt::print(fg(fmt::color::red), "Failed to initialize Renderer!\nReason: No window provided!\n");
             return;
         }
+
+        m_window = window;
+
+        // glDepthRangef(0.01, 100);
+
+        glEnable(GL_PROGRAM_POINT_SIZE);
+        enableDepthTest();
     }
 
     enum class DRAW_TYPE
@@ -26,29 +31,67 @@ public:
         INDICES
     };
 
-    auto render(const VertexArray& vao, const Shader& shader, DRAW_TYPE type, GLenum render_mode) const noexcept -> void {
-        shader.useShader();
-        vao.bind();
 
-        if (type == DRAW_TYPE::ARRAYS)
-        {
-            glDrawArrays(render_mode, 0, 3);
-        }
-        else if (type == DRAW_TYPE::INDICES)
-        {
-            glDrawElements(render_mode, 36, GL_UNSIGNED_INT, 0);
-        }
+    // auto render(const VertexArray& vao, const Shader& shader, DRAW_TYPE type, GLenum render_mode) const noexcept -> void {
+    //     shader.useShader();
+    //     vao.bind();
+
+    //     if (type == DRAW_TYPE::ARRAYS)
+    //     {
+    //         glDrawArrays(render_mode, 0, 3);
+    //     }
+    //     else if (type == DRAW_TYPE::INDICES)
+    //     {
+    //         glDrawElements(render_mode, 36, GL_UNSIGNED_INT, 0);
+    //     }
+    // }
+
+    auto render(std::shared_ptr<Mesh>& mesh) -> void
+    {
+        mesh->getShader()->useShader();
+        mesh->getVao()->bind();
+
+        glDrawElements(mesh->getRenderMode(), mesh->getIndicesCount(), GL_UNSIGNED_INT, nullptr);
     }
 
     auto clear(glm::vec4 clear_color = {1.0f, 1.0f, 1.0f, 1.0f}) const noexcept -> void {
+
+        // glClearColor(0.902, 0.878, 0.796, 1);
         glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
-        glClear(GL_COLOR_BUFFER_BIT);
+
+        if (m_depth_test)
+        {
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        }
+        else
+        {
+            glClear(GL_COLOR_BUFFER_BIT);
+        }
+    }
+
+    auto submit(std::pair<std::shared_ptr<VertexArray>, std::shared_ptr<Shader>> drawable_data) -> void
+    {
+        m_drawable_list.emplace_back(drawable_data);
+    }
+
+    auto enableDepthTest() -> void
+    {
+        glEnable(GL_DEPTH_TEST);
+        m_depth_test = true;
+    }
+
+    auto disableDepthTest() -> void
+    {
+        glDisable(GL_DEPTH_TEST);
+        m_depth_test = false;
     }
        
     ~Renderer() = default;
 
 private:
     GLFWwindow* m_window;
+    std::vector<std::pair<std::shared_ptr<VertexArray>, std::shared_ptr<Shader>>> m_drawable_list;
+    bool m_depth_test;
 };
 
 
