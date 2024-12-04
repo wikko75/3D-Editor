36e5a1066e08020c28aaf840463666f33c9242fc
Wiktor <wiktorkar25@gmail.com>
2024-12-04 22:31:29 +0100

    Add point light to the scene

--- App/assets/shaders/light_vertex.glsl
+++ App/assets/shaders/light_vertex.glsl
@@ -1,12 +1,15 @@
 #version 330 core
 
-layout (location = 0) in vec3 in_pos = vec3(1.0, 0.0, 0.0);
+layout (location = 0) in vec3 in_pos;
+layout (location = 2) in vec4 in_color;
 
-uniform vec3 model_mtx;
-uniform vec3 u_view_projection_mtx;
+uniform mat4 u_model_mtx;
+uniform mat4 u_view_projection_mtx;
 
+out vec4 color;
 
 void main() {
-    gl_Position = u_view_projection_mtx * model_mtx * vec4(in_pos, 1.0);
+    gl_Position = u_view_projection_mtx * u_model_mtx * vec4(in_pos, 1.0);
+    color = in_color;
 }
 
